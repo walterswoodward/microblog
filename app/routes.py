00127d6d0959b25fbf2aefbf874ceefbca8ad0dd
walterswoodward <walter.sekope.woodward@gmail.com>
2021-03-24 14:21:51 -0700

    Adds templates

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from app import app
 
 # these are Python decorators
@@ -6,4 +7,21 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
+    posts = [
+        {
+            'author': {'username': 'Gandhi'},
+            'body': 'Happiness is when what you think, what you say, and what you do are in harmony.'
+        },
+        {
+            'author': {'username': 'Mother Teresa'},
+            'body': 'If you judge people, you have no time to love them.'
+        }
+    ]
+
+    # Note the conditional statements in index.html that ensure user, if undefined
+    # defaults to 'Stranger'
+    return render_template('index.html', title='Home', user={'username': 'Walter'}, posts=posts)
+
+@app.route('/hello')
+def hello():
     return "Hello, World!"
